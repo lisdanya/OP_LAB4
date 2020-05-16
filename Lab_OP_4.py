69bed0a95c912198a245dfb198b074d2366f0d92
MIDAS26 <56016042+MIDAS26@users.noreply.github.com>
2020-05-16 12:17:59 +0300

    Add files via upload

--- Lab_OP_4.py
+++ Lab_OP_4.py
@@ -1,3 +1,6 @@
+from sys import argv
+
+
 def encoder(data, dict_size):
     dictionary = dict((chr(i), i) for i in range(dict_size))
     str = ""
@@ -15,11 +18,48 @@ def encoder(data, dict_size):
         lst.append(dictionary[str])
     return lst
 
+
+def decoder(data, dict_size):
+    dictionary = dict((i, chr(i)) for i in range(dict_size))
+    lst = []
+    str = chr(data.pop(0))
+    lst.append(str)
+    for key in data:
+        if key in dictionary:
+            entry = dictionary[key]
+        elif key == dict_size:
+            entry = str + str[0]
+        lst.append(entry)
+        dictionary[dict_size] = str + entry[0]
+        dict_size += 1
+        str = entry
+    s = ''
+    for i in range(len(lst)):
+        s = s + lst[i]
+    return s
+
+
+# data = open(str(argv[2]))
 name_file=str(argv[2])
 data = open(name_file)
 dict_size = 256
 print(data)
 for s in data:
     print(s)
-print(encoder(s,dict_size))
-    
+
+
+out = name_file.split(".")[0]
+output_file = open(out + ".lzw", "w")
+
+
+if str(argv[1]) == "e":
+    encoded = encoder(s, dict_size)
+
+    print(encoded)
+    output_file.write(str(encoded))
+# elif str(argv[1])=="d":
+#     decoded = decoder(encoded, dict_size)
+#     print(decoded)
+
+
+
